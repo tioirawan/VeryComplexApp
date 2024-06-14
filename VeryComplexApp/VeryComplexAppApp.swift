0cfbe8e6c5461add64fbbdf2ef39547a5c703364
Tio Irawan <sattanationmail@gmail.com>
2024-06-14 21:34:14 +0700

    Initial Commit

---
+++ VeryComplexApp/VeryComplexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeryComplexAppApp.swift
+//  VeryComplexApp
+//
+//  Created by mac on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct VeryComplexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
